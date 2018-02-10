e75ec027b8fb23d5e75caac599b7833baf82df1c
Thibault HENTGES <thibault.hentges@gmail.com>
2018-02-10 21:36:32 +0100

    refactor

--- app/controllers/users_ctrl.js
+++ app/controllers/users_ctrl.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 const models = require('../models');
 const usersService = require('../services/users_service');
 
-//TODO make middlewares
 userRouter = express.Router();
 
 userRouter.post('/', (req, res, next) => {
@@ -21,11 +20,14 @@ userRouter.post('/', (req, res, next) => {
     );
 });
 
-userRouter.get('/:id', (req, res, next) => {
+// MDLWRE
+userRouter.use('/id/:id', (req, res, next) => {
     models.User.findById(req.params.id).then(
         function(user){
-            if (user)
-                res.send(user);
+            if (user) {
+                res.locals.user = user;
+                next();
+            }
             else
                 throw res.status(404).send('no user found with id : ' + req.params.id);
 
@@ -36,16 +38,35 @@ userRouter.get('/:id', (req, res, next) => {
     );
 });
 
+userRouter.get('/id/:id', (req, res,next) => {
+    res.send(res.locals.user);
+});
 
-// get a user by given :prop with the given :value
-userRouter.get('/:prop/:value', (req, res, next) => {
+userRouter.put('/id/:id/:prop/:value', (req, res,next) => {
+    array = {};
+    array[req.params.prop] = req.params.value;
+    res.locals.user.updateAttributes(array).then(
+        function(success){
+            res.send(success);
+        },
+        function(error){
+            res.status(400).send('error updating user');
+        }
+    );
+
+});
+
+// MDLWARE
+userRouter.use('/:prop/:value', (req, res, next) => {
     var array = {};
     array[req.params.prop] = req.params.value;
 
     models.User.findOne({ where: array }).then(
         function(user){
-            if (user)
-                res.send(user);
+            if (user) {
+                res.locals.user = user;
+                next();
+            }
             else
                 throw res.status(404).send('no user found with '+ req.params.prop +' : ' + req.params.value);
 
@@ -54,25 +75,17 @@ userRouter.get('/:prop/:value', (req, res, next) => {
             throw res.status(400).send('error server');
         }
     );
+});
+
+// get a user by given :prop with the given :value
+userRouter.get('/:prop/:value', (req, res, next) => {
+    res.send(res.locals.user);
 });
 
 // get the :only property of the user found with the given :value for a given prop
 userRouter.get('/:prop/:value/:only', (req, res, next) => {
-    var array = {};
-    array[req.params.prop] = req.params.value;
-
-    models.User.findOne({ where: array }).then(
-        function(user){
-            if (user && user[req.params.only])
-                res.send(user[req.params.only]);
-            else
-                throw res.status(404).send('no user found with '+ req.params.prop +' : ' + req.params.value);
-
-        },
-        function(error){
-            throw res.status(400).send('error server');
-        }
-    );
+    res.send(res.locals.user[req.params.only]);
 });
+
 
 module.exports = userRouter;
