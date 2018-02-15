d3963597fe1fc8ebd957921052ceea1d8a366cde
Thibault HENTGES <thibault.hentges@gmail.com>
2018-02-15 22:30:34 +0100

    refactoring

--- test/users/controllers_test.js
+++ test/users/controllers_test.js
@@ -3,9 +3,9 @@ process.env.NODE_ENV = 'test';
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const chaiAsPromised = require('chai-as-promised');
-const app = require('../server');
-const models = require('../models');
-const usersService = require('../services/users_service');
+const app = require('../../server');
+const models = require('../../models');
+const usersService = require('../../services/users_service');
 
 const should = chai.should();
 
@@ -160,37 +160,6 @@ describe('Users', () => {
                 });
             });
         });
-    });
-
-    describe('SERVICES' , () => {
-
-        describe('[create]  _ creating a user', () => {
-            it('it should encrypt password in DB', (done) => {
-                usersService.create("Bob", "Bob@gmail.com", "password").then(
-                    (user) => {
-                        user.password.should.not.eql('password');
-                        user.password.indexOf('$2a$10').should.eql(0);
-                        done();
-                    }
-                );
-            });
-
-            it('it should not create a user (password validation)', (done) => {
-                usersService.create("Bob", "Bob@gmail.com", "abc").should.be.rejected.and.notify(done);
-            });
-
-            it('it should not create a user (email validation)', (done) => {
-                // invalid email
-                usersService.create("Bob", "not an email", "password").should.be.rejected;
-
-                usersService.create("Bob", "Bob@gmail.com", "password").then(
-                    () => {
-                        usersService.create("Bobby", "Bob@gmail.com", "password").should.be.rejected.and.notify(done);
-                    }
-                );
-            });
-        });
-
     });
 
 });
---
+++ test/users/models_test.js
@@ -0,0 +1,57 @@
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const chaiAsPromised = require('chai-as-promised');
+const app = require('../../server');
+const models = require('../../models');
+const usersService = require('../../services/users_service');
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+chai.use(chaiAsPromised);
+
+describe('Users', () => {
+
+    beforeEach((done) => { // empty db b4 each test
+        models.sequelize.sync({force: true}).then(
+            () => {
+                done();
+            }
+        );
+    });
+
+    describe('SERVICES (Models)' , () => {
+
+        describe('[create]  _ creating a user', () => {
+            it('it should encrypt password in DB', (done) => {
+                usersService.create("Bob", "Bob@gmail.com", "password").then(
+                    (user) => {
+                        user.password.should.not.eql('password');
+                        user.password.indexOf('$2a$10').should.eql(0);
+                        done();
+                    }
+                );
+            });
+
+            it('it should not create a user (password validation)', (done) => {
+                usersService.create("Bob", "Bob@gmail.com", "abc").should.be.rejected.and.notify(done);
+            });
+
+            it('it should not create a user (email validation)', (done) => {
+                // invalid email
+                usersService.create("Bob", "not an email", "password").should.be.rejected;
+
+                // already used email (constraint)
+                usersService.create("Bob", "Bob@gmail.com", "password").then(
+                    () => {
+                        usersService.create("Bobby", "Bob@gmail.com", "password").should.be.rejected.and.notify(done);
+                    }
+                );
+            });
+        });
+
+    });
+
+});
